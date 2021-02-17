1857972e991f0f752fbdd720172387480c6dd8e5
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-17 12:14:25 +0100

    setting up db connection to mongodb

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public')); //common name for public folder
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to DB'));
+
+
+
+
 app.listen(process.env.PORT || 3000);
 
 
