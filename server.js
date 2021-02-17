e849441698a24412c90f2dcf6c2f7fa57f1334a0
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-17 14:55:55 +0100

    connecting dotenv variables

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== 'production') { //defaulted
+    require('dotenv').config();
+}
+
+
 const express = require('express');
 const app = express();
 
@@ -22,7 +27,7 @@ db.once('open', () => console.log('Connected to DB'));
 
 
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
 
 
 //routers
