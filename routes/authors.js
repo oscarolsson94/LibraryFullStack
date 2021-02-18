503949bf17e5edc6a5c28f25a2f1bbc2f3a20804
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-18 10:04:25 +0100

    displaying all authors

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const Author = require('../models/author');
 
 // All authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    try {
+        const authors = await Author.find({}); // get all authors with empty obj
+        res.render('authors/index', { authors: authors });
+    } catch {
+        res.redirect('/');
+    };
 });
 
 // New Author Route
