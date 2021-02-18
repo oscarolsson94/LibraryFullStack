722e55d8efa5e89455dd60055c4a5cfd21bbbed3
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-18 09:40:17 +0100

    working on creating new authors

--- routes/authors.js
+++ routes/authors.js
@@ -14,7 +14,21 @@ router.get('/new', (req, res) => {
 
 // Create Author Route
 router.post('/', () => {
-    res.send('Create');
+    const author = new Author({
+        name: req.body.name
+    });
+    author.save((err, newAuthor) => {
+        if (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            });
+        } else {
+            // res.redirect(`authors/${newAuthor.id}`);
+            res.redirect(`authors`);
+        }
+    });
+    res.send(req.body.name);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const express = require('express');
 const app = express();
 
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); //where our views are coming from
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public')); //common name for public folder
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
